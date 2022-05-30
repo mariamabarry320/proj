67a9fc3a0596907dad2ca3369e6e4cf04c70eaf9
mariamabarry320 <mariamabailobarry2@gmail.com>
2022-05-30 10:35:08 +0000

    second code

---
+++ main.py
@@ -0,0 +1,379 @@
+# userName = "Bah"
+# Number = 5
+# Number2 = 7
+# result = Number + Number2
+# print(result)
+
+# x = 25
+# y = 20
+# z = 3
+# f = 2
+# j = (x - y) * z/f
+# print(j)
+# print("hello world")
+# input("what's your name")
+# my_age = 12
+# my_age += 4
+# print(my_age)
+
+# question 1
+# x = 10
+# x = 20
+# x = 15
+# print(x)
+
+# question 2
+# x = 4
+# y = 2
+# z = int(x/y)
+# print(z)
+
+# question 3
+# l = 2
+# b = 3
+# a = 2*l + 2*b
+# p = l * b
+# print(a)
+# print(p)
+
+# question 4
+# x = 25
+# y = 20
+# z = 3
+# f = 2
+# result = (x-y) * z/f
+# print(result)
+#
+# question 5
+# v = 20
+# j = 20 % 2
+# print(j)
+#
+# question 5
+# x = 5 ** 2
+# import math
+# j = int(math.sqrt(x))
+# print(j)
+#
+# question 6
+# p = 3
+# v = p**3
+# SA = 6*p ** 2
+# print(v)
+# print(SA)
+#
+# question 7
+# x = 5
+# x += 3
+# print(x)
+#
+# question 8
+# x = 11
+# y = 5
+# z = x*y
+# print(z)
+#
+# question 9
+# x = 5
+# y = 10
+# z = (x-y) * (x+y)
+# print(z)
+#
+#
+# x = 5
+# y = 10
+# z = abs(x-y) * (x+y)
+# print(z)
+#
+# question 11
+# a = 5
+# b = 7
+# h = 4
+# area = 1/2 * b * h
+# perimeter = 2*a + b
+# print(area)
+# print(perimeter)
+#
+# question 12
+# import math
+# radius = float(input('Enter the radius of the circle: '))
+# area = int(math.pi * radius**2)
+# perimeter = int(2 * math.pi * radius)
+# print(area)
+# print(perimeter)
+#
+# question 13
+# import math
+# radius = float(input('enter the radius of the cylinder: '))
+# height = float(input('enter the height of the radius: '))
+# area = 2 * math.pi * radius * height
+# perimeter = 2*math.pi*radius*height
+# print(area)
+# print(perimeter)
+#
+# question 14
+# import math
+# radius = float(input('enter the radius of the cone: '))
+# height = float(input('enter the height of the cone: '))
+# length = float(input('enter the length of the cone: '))
+# area = int((1/3)*math.pi*radius**2*height)
+# volume = int(math.pi*radius*height)
+# perimeter = int(radius*math.pi*(radius+length))
+# print(area)
+# print(volume)
+# print(perimeter)
+#
+# question 15
+# import math
+# radius = float(input('enter the rt('enter the height of the cone: '))
+# # area = int((1/3)*math.pi*radius**2*height)
+# # volume = int(2*math.pi*radius**2)
+# # perimeter = int(3*math.pi*radius**2)
+# # print(area)
+# # print(volume)
+# # print(perimeter)
+# #
+# # ASSIGNMENT QUESTION
+# #
+# # quadratic equation = a*x**2 + b*x + c = radius of the cone: '))
+#
+# import math
+# a = int(input('input value a: '))
+# b = int(input('input value b: '))
+# c = int(input('input value c: '))
+# #
+# # calculate the discriminant
+# d = (b**2) - (4*a*c)
+# #
+# # calculate the formula
+# solution1 = ((b*-1) + math.sqrt(d))/(2*a)
+# solution2 = ((-1*b) - math.sqrt(d))/(2*a)
+#
+# print(solution1)
+# print(solution2)
+#
+#
+#
+#
+# name = 'bah'
+# print(type(name))
+#
+# name = input('what is your name?: ')
+# age = int(input(' what is yor age?: '))
+# school = input('what school are u from?: ')
+# print(f'Your name is {name} \n you are {age} \n you are in {school}')
+#
+# age = int(input('what is your age: '))
+# age_remaining = 90 - age
+# remaining_days = age_remaining * 365
+# remaining_months = age_remaining * 12
+# remaining_weeks = age_remaining * 52
+#
+# print(f'Your age is {age} \n you have {remaining_days} days to live \n {remaining_months} months to live \n'
+#       f' and {remaining_weeks} weeks to live')
+# if age > 23 and age < 35:
+#     print("make best use of your life")
+#
+# ASSIGNMENT TWO
+#
+# QUESTION ONE
+# kilometers = float(input('enter value in kilometers: '))
+# conversion = 0.621371
+# miles = kilometers * conversion
+# print(' %0.2f kilometers is equal to %0.2f ,miles' %(kilometers,miles))
+#
+# QUESTION FOUR
+# x = int(input('enter value of x: '))
+# y = int(input('enter value of y: '))
+# summation = x + y
+# multiplication = x * y
+# print(f' the sum of the integers is {summation} and the multiplication {multiplication}')
+#
+# QUESTION FIVE
+# number = int(input('enter the value of the number: '))
+# cube = number * number * number
+# square = number ** 2
+# print(f' {number} has a cube value of {cube} and a square value of {square}')
+#
+# QUESTION SIX
+# name = 'new york'
+# celsius = int(input('enter celsius value: '))
+# fahrenheit = int((celsius * 9/5) + 32)
+# print(f' {name} has a {celsius} degree celsius and {fahrenheit} ')
+#
+# QUESTION SEVEN
+# length = int(input('enter the value of length: '))
+# width = int(input('enter value of width: '))
+# area = length * width
+# perimeter = 2*width + 2*length
+# print(f' The rectangle has an area of {area} and a perimeter of {perimeter}')
+#
+# QUESTION EIGHT
+# import math
+# radius = int(input('enter radius of a circle: '))
+# area = int(math.pi * radius**2)
+# diameter = 2 * radius
+# circumference = int(2 * math.pi * radius)
+# print(f'The circle has an area of {area},a diameter of {diameter} and {circumference} circumference ')
+#
+# QUESTION TWO
+# import calendar
+# year = 2022
+# month = 5
+# print(calendar.month(year,month))
+#
+# QUESTION THREE
+# import calendar
+# year = input('enter the year: ')
+# month = input('enter the month: ')
+# print(calendar.month(2022, 5))
+#
+#
+# print("hello")
+#
+# STRINGS TRY ME
+#
+# QUESTION ONE
+# name = "Mitch"
+# age = 12
+# full_time_employee = "True"
+# print(f'{name},{age},{full_time_employee}')
+#
+# name = "Mitch"
+# age = 26
+# full_time_employee = True
+# print(name, age, full_time_employee)
+#
+# QUESTION TWO
+# x = 20
+# y = 4
+# print(x - y)
+#
+# QUESTION THREE
+# name = input("Enter your name ")
+# x = name.split(" ")
+# print(type(x))
+# print(f' hello  whats  {x} ')
+
+# QUESTION FOUR
+# Name = input('Enter your name: ')
+# variable = Name.upper()
+# print(variable)
+#
+# QUESTION FIVE
+# x = input('Enter your email: ')
+# print(x)
+
+# x = "Hello" + "mariama"
+# print(x)
+# speech = "she said: \"HI\""
+# print(speech)
+#
+# days = 365
+# print(f'There are {days} days in a year')
+# n = 3.55
+# x = type(n)
+# print(x)
+
+# x = 55
+# x *= 11
+# print(x)
+#
+#
+# STRINGS TRY Me
+#
+# QUESTION ONE(1)
+# name = "Mitch"
+# age = 26
+# full_time_employee = True
+# print(name, age, full_time_employee)
+#
+# QUESTION TWO(2)
+# x = 20
+# y = 4
+# print(x - y)
+#
+# QUESTION THREE(3)
+# name = input('Enter your name: ')
+# x = name.split()
+# print(x)
+#
+# QUESTION FOUR(4)
+# user = input('Enter your name:')
+# name = user.upper()
+# print(name)
+#
+# QUESTION FIVE(5) and SIX(6)
+# email = input('Enter your email:')
+# user_name = email.split("@")[0]
+# print(user_name)
+# print("hello", user_name)
+#
+# QUESTION SEVEN(7)
+# firstname = input('enter your firstname:' )
+# lastname = input('enter your lastname: ')
+# age = input('enter your age: ')
+# print(firstname, lastname, age)
+#
+# QUESTION EIGHT(8)
+# first_name = input('enter first_name:')
+# last_name = input('enter last_name: ')
+# age = input('enter age: ')
+# name = first_name + " " + last_name
+# print(name + "\n" + age)
+#
+# QUESTION NINE(9) & TEN(10)
+# email = input('enter your email: ')
+# user_name = email.split("@")[0]
+# print(len(user_name))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
